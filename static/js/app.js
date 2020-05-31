52c26fe5feaf12f83f2f4c46acf3ae1815066b0b
nic britz <nico.britz@outlook.com>
2020-05-31 11:28:23 +0100

    :sparkles: added main page slider template

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,18 @@
 // On DOM Loaded
 document.addEventListener('DOMContentLoaded', function () {
-    //Nav-bar for mobile Init
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    // Nav-bar for mobile Init
+    let sidenavElems = document.querySelectorAll('.sidenav');
+    let sidenavInstances = M.Sidenav.init(sidenavElems);
+
+    // Carousel init
+    let carouselElems = document.querySelectorAll('.carousel');
+    let carouselInstances = M.Carousel.init(carouselElems);
+    let carouselInstance = M.Carousel.init({
+        fullWidth: true,
+        indicators: true
+    });
+
+    // Slider init
+    let sliderElems = document.querySelectorAll('.slider');
+    let instances = M.Slider.init(sliderElems);
 });
