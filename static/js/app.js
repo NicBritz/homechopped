36e7da6f22424004e1cb4e3c62f9c2b4195d290d
nic britz <nico.britz@outlook.com>
2020-05-31 15:43:36 +0100

    :sparkels: added recipe cards to the index page

--- static/js/app.js
+++ static/js/app.js
@@ -4,15 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let sidenavElems = document.querySelectorAll('.sidenav');
     let sidenavInstances = M.Sidenav.init(sidenavElems);
 
-    // Carousel init
-    let carouselElems = document.querySelectorAll('.carousel');
-    let carouselInstances = M.Carousel.init(carouselElems);
-    let carouselInstance = M.Carousel.init({
-        fullWidth: true,
-        indicators: true
-    });
-
     // Slider init
     let sliderElems = document.querySelectorAll('.slider');
     let instances = M.Slider.init(sliderElems);
+
 });
