380399b3036e51b740b5f89685f6c363a385d33e
nic britz <nico.britz@outlook.com>
2020-06-01 18:59:17 +0100

    :sparkles: aded basic functioning featured recipe page with sorting

--- app.py
+++ app.py
@@ -13,7 +13,23 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', recipes=mongo.db.recipes.find(), categories=mongo.db.categories.find())
+    # Returns the index.html and passes in all recipes from the database
+    all_recipes = mongo.db.recipes.find()
+    return render_template('index.html', recipes=all_recipes)
+
+
+@app.route('/featured/<sortby>')
+def featured_sorted(sortby):
+    # Returns the featured.html and passes in featured recipes from the database
+    # with a variable for some sorting options
+    featured_recipes = None
+    if sortby == 'a-z':
+        featured_recipes = mongo.db.recipes.find({'featured': 'true'}).sort([('name', 1)])
+        a_to_z = 'a-z'
+    elif sortby == 'z-a':
+        featured_recipes = mongo.db.recipes.find({'featured': 'true'}).sort([('name', -1)])
+        a_to_z = 'z-a'
+    return render_template('featured.html', recipes=featured_recipes, a_to_z=a_to_z)
 
 
 if __name__ == '__main__':
