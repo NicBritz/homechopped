e0807d1a629f681b742e168f0bf1cfb78b73c4d3
nico britz <nico.britz@outlook.com>
2020-06-04 14:20:03 +0100

    :hammer: refactored and tidied up login code and templates

--- app.py
+++ app.py
@@ -19,22 +19,20 @@ mongo = PyMongo(app)
 def index():
     # Returns the index.html and passes in all recipes from the database
     all_recipes = mongo.db.recipes.find()
-
-    # SRC - https://www.youtube.com/watch?v=vVx1737auSE && https://www.youtube.com/watch?v=PYILMiGxpAU
-
     return render_template('index.html', recipes=all_recipes, )
 
 
 # login routes
-
-# sign in
+#
+# Sign in page
 @app.route('/sign-in', methods=['POST', 'GET'])
 def sign_in():
     # This function renders the sign in template and checks the users credentials against the database.
+    #
+    # SRC - https://www.youtube.com/watch?v=vVx1737auSE && https://www.youtube.com/watch?v=PYILMiGxpAU
 
     # Creates a blank feedback message
     message = ''
-
     if request.method == 'POST':
 
         # search the database for the username
@@ -50,49 +48,73 @@ def sign_in():
         # authentication failed
         message = 'Invalid username or password.'
 
-    # if the request is a get request render the sign in template
+    # if the request is a get then render the sign in template
     return render_template('sign-in.html', message=message)
 
 
-# register
+# Registration Page
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    # This function renders the registration template and adds users credentials to the database.
+    #
+    # SRC - https://www.youtube.com/watch?v=vVx1737auSE && https://www.youtube.com/watch?v=PYILMiGxpAU
+
+    # Creates a blank feedback message
+    message = ''
     if request.method == 'POST':
+        # create a variable of the users database
         db_users = mongo.db.users
+
         # check if user already exists
         existing_user = db_users.find_one({'username': request.form['username']})
+
         # if the user does not already exist
         if existing_user is None:
+
+            # if password is less than 5 characters
+            if len(request.form['password']) < 5:
+                message = 'Password must be at least 5 characters long.'
+                return render_template('register.html', message=message)
+            # Hash the password for better security
             hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            # add to mongodb
+            # add record to mongodb
             db_users.insert({'username': request.form['username'], 'password': hashpass})
             session['USERNAME'] = request.form['username']
             return redirect(url_for('profile'))
+        # if the user already exists
         else:
-            return 'that username already exists!'
+            message = 'username already exists.'
+            return render_template('register.html', message=message)
+
     # if it is a get request return registration template
-    return render_template('register.html')
+    return render_template('register.html', message=message)
 
 
+# Profile page
 @app.route('/profile')
 def profile():
+    # Directs the user to the main profile page
+    #
+    # create a variable of the users database
     db_users = mongo.db.users
-    # passed what it will become if not exist == None
-    # if user exists
+
+    # if user exists in the database and in the session variable
     if session.get('USERNAME', None) is not None:
-        username = session.get("USERNAME")
+        # assigns session var to username variable
+        username = session['USERNAME']
+
         # get the user from the database
         existing_user = db_users.find_one({'username': username})
-        print(existing_user)
-        # userdata = users[username]
+
         return render_template('profile.html', userdata=existing_user)
     else:
-        print('username not found in session')
+        # if the user is not signed the are redirected to the sign in page
         return redirect(url_for('sign_in'))
 
-
+# Sign out
 @app.route('/sign-out')
 def sign_out():
+    # clears the session variables and redirects to the sign in page
     session.clear()
     return redirect(url_for('sign_in'))
 
